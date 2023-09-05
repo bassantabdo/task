a56c3e337669bedb0eca0472b795ed27efd2d440
basantabdo514 <basantabdo514@gmail.com>
2023-09-05 16:15:38 +0300

    first commit

---
+++ task.js
@@ -0,0 +1,14 @@
+var allproducts = document.querySelector(".bottum")
+
+var allproducts = document.querySelector(".my-cart")
+
+var bottum = document.querySelector(".price")
+
+allproducts.forEach(function(iphone){
+
+    iphone.onclick = function()
+    {
+      my-cart.innerHTML += iphone.textContent + <br>
+    }
+
+})
